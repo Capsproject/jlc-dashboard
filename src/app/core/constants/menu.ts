efa65a08080d6c20d9222b8df2f854ffe2992208
Mark Lowel Montealto <mark.m@oisooh.com>
2024-10-11 00:24:09 +0800

    update menu  ts

--- src/app/core/constants/menu.ts
+++ src/app/core/constants/menu.ts
@@ -20,31 +20,38 @@ export class Menu {
       roles: ['admin'],
       items: [
         {
-          icon: 'icons/heroicons/outline/download.svg',
-          label: '',
+          icon: 'icons/heroicons/solid/document.svg',
+          label: 'Job Orders',
           route: '/download',
         },
         {
           icon: 'icons/heroicons/outline/gift.svg',
-          label: 'Gift Card',
+          label: 'Archive',
           route: '/gift',
         },
       ],
     },
     {
-      group: 'All',
+      group: 'Manage Work',
       separator: false,
       roles: ['manager'],
       items: [
         {
           icon: 'icons/heroicons/outline/cog.svg',
-          label: 'Settings',
+          label: 'Job Order Dashboard',
           route: '/settings',
         },
+      ],
+    },
+    {
+      group: 'Manage Records',
+      separator: true,
+      roles: ['manager'],
+      items: [
         {
-          icon: 'icons/heroicons/outline/bell.svg',
-          label: 'Notifications',
-          route: '/gift',
+          icon: 'icons/heroicons/outline/document-text.svg',
+          label: 'Customer Dashboard',
+          route: '/records',
         },
       ],
     },
@@ -69,6 +76,18 @@ export class Menu {
           icon: 'icons/heroicons/outline/chart-pie.svg',
           label: 'Analytics Dashboard',
           route: '/owner/dashboard',
+        },
+      ]
+    },
+    {
+      group: 'Tasks',
+      separator: true,
+      roles: ['technician'],
+      items: [
+        {
+          icon: 'icons/heroicons/outline/check-circle.svg',
+          label: 'Tasks',
+          route: '/tasks',
         },
       ]
     }
